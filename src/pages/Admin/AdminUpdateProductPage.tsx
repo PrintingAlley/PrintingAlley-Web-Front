dfedb714d18b944e67b6d2e76e024a38bf1c2714
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-11-14 10:33:53 +0900

    🛠️ :: navigate replace 설정 적용

--- src/pages/Admin/AdminUpdateProductPage.tsx
+++ src/pages/Admin/AdminUpdateProductPage.tsx
@@ -22,7 +22,7 @@ export default function AdminUpdateProductPage() {
   };
 
   const navigateToProductDetail = () => {
-    navigate(`/product/${id}`);
+    navigate(`/product/${id}`, { replace: true });
   };
 
   useEffect(() => {
